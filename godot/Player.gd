3a48a42e9a8b39c73505426bf15dde0fa5f18cd2
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-05-10 20:22:31 +0200

    movement and collide

--- godot/Player.gd
+++ godot/Player.gd
@@ -1,13 +1,28 @@
-extends Node2D
+extends KinematicBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var x_speed = 50
+var gravity = 50
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.	
+
+func die():
+	self.queue_free()
+	
+func movement():
+	var movement = Vector2()
+	movement.y += gravity
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= x_speed
+	elif Input.is_action_pressed("ui_right"):
+		movement.x += x_speed
+	move_and_slide(movement)
+		
+func _process(delta):
+	movement()
+	
