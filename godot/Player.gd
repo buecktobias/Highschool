3f23cb1e7b529cd345db5586a6720dccf4fa3440
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-05-10 18:56:51 +0200

    new file structure

--- godot/Player.gd
+++ godot/Player.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends Node2D
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,6 +9,5 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		rect_position += Vector2(-5, 0)
+#func _process(delta):
+#	pass
