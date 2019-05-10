af368c9ceac2fdfbb4429b749e455893495c5aad
buecktobias <44665917+buecktobias@users.noreply.github.com>
2019-05-10 22:16:24 +0200

    moving obstacles

--- godot/MovingAbstacle.gd
+++ godot/MovingAbstacle.gd
@@ -3,15 +3,18 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var x_speed = 0
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	x_speed = rand_range(-5,5)
+	
+	
+func _process(delta):
+	position.x += x_speed
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-
-func _on_StaticObstacle_body_entered(body):
+func _on_MovingObstacle_body_entered(body):
 	if body.is_in_group("Player"):
 		var player = body
 		player.lose()
