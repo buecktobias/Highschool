deed530ce58d0c6f1643a74e2fecd5d13bdb27c9
Torben Wetter <torben@wetter.codes>
2019-05-11 17:30:32 +0200

    Added score

--- godot/MainCamera.gd
+++ godot/MainCamera.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
-export (NodePath) var player_path
 export (int) var offset_to_player = 200
+export (NodePath) var player_path
+export (NodePath) var score_path
 
 var player
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(player_path)
+	score = get_node(score_path)
 	ScreenEffects.submit_shaker($Shaker)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.y = max(player.position.y + offset_to_player, position.y)
+	score.rect_position.y = position.y - 385
---
+++ godot/Score.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+export (NodePath) var player_path
+var player
+
+const format_string = "[b][color=yellow]%s[/color][/b]"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_node(player_path)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	bbcode_text = format_string % str(round(player.y_speed))
